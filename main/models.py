b28b484f851e4dd0ee05775cbb1b46a64c8b8733
haku <andrejosipov19120@gmail.com>
2023-09-21 09:55:22 +0300

    created base for that project

---
+++ main/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.TextField()
+    description = models.CharField()
---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def index(request):
+    template = 'homepage/index.html'
+    context = {}
+    return render(request, template, context)
