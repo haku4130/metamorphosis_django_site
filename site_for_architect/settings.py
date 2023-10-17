0c25867b34ab6a8adf7a7b4d83d1b6744b9f1815
haku <andrejosipov19120@gmail.com>
2023-10-17 12:44:20 +0000

    modified settings for production

--- site_for_architect/settings.py
+++ site_for_architect/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zkw5dkpbeg7&nha%iu&qr7(e2(-!zjqbdsgi%o89c1c0t42fy*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['metamorphosis1.pythonanywhere.com']
 
 # Application definition
 
