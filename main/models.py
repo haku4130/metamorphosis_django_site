947331ee27e481c67c185937d85bc5d132082e69
haku4130 <andrejosipov19120@gmail.com>
2023-11-09 00:02:19 +0300

    Изменяю поля моделей для того, чтобы можно было выбирать порядок отображения фото в проекте и фото для главной страницы

--- main/models.py
+++ main/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class ProjectsPhoto(models.Model):
     image = models.ImageField(upload_to='static/projects_photos/')
     description = models.TextField(blank=True)
+    display_order = models.PositiveIntegerField(default=0)
+
+    is_main = models.BooleanField(default=False, verbose_name=_("Show as main photo on homepage"))
+
+    def clean(self):
+        # Если фотография установлена как основная, убедиться, что нет других фото с is_main=True для этого проекта
+        if self.is_main:
+            main_photos = ProjectsPhoto.objects.filter(
+                is_main=True,
+                project_photos__in=Project.objects.filter(photos=self)
+            ).exclude(id=self.id)
+            if main_photos.exists():
+                raise ValidationError(_("There can be only one main photo per project."))
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.image.name
@@ -20,8 +39,9 @@ class ProjectTheme(models.Model):
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    theme = models.ManyToManyField(ProjectTheme, null=True)
-    photos = models.ManyToManyField(ProjectsPhoto)
+    theme = models.ManyToManyField(ProjectTheme, blank=True)
+    photos = models.ManyToManyField(ProjectsPhoto, related_name='project_photos')
+    main_photo = models.OneToOneField(ProjectsPhoto, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
